9de3e15aec8482578cb2ae06ffe980953c99bd94
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-01-27 19:00:34 +0100

    Refactored

--- Men_from_boys_array_sort.rb
+++ Men_from_boys_array_sort.rb
@@ -12,11 +12,9 @@ def men_from_boys(arr)
   even.sort + odd.sort.reverse
 end
 
-
-
 # --------- refactored version ------------
 
-def men_from_boys(arr)
+def men_from_boys_refactored(arr)
   evens, odds = arr.uniq.partition &:even?
   evens.sort + odds.sort_by(&:-@)
 end
